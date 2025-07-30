eec28760afbb7f5113fd5a55b694639affeee627
TRDR <trdr@trdrs-mbp.mynetworksettings.com>
2025-07-29 22:53:29 -0400

    pushing changes remotely

---
+++ moviesBackend/src/test/java/com/kae_ptl/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kae_ptl.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
